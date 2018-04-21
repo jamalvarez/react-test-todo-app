f49b0d2c2711a3035848127ee969be5240205ed5
Jesús Marín <ext_jamarina@Falabella.cl>
2018-04-21 13:05:19 -0300

    small changes to markup and design

--- src/TodoApp/TodoList/Todo.jsx
+++ src/TodoApp/TodoList/Todo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Todo = ({ todo, toggle, remove }) => {
+  const statusButtonText = todo.done ? 'Done!' : 'Pending';
   return (
     <div className={'todo' + (todo.done ? ' done' : '')}>
       <div className='text'>
         <h2>{todo.text}</h2>
       </div>
-        <div className='checkbox' onClick={toggle}>
-          <input type='checkbox' checked={todo.done}/>
+        <div className='toggle-button' onClick={toggle}>
+          <h2>{statusButtonText} </h2>
       </div>
       <div className='remove' onClick={remove}>
         <span>Remove</span>
